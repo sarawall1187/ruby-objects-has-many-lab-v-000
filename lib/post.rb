18cc5975e53359b680054526820562bfffbe3f0d
Sara Walker <sarawall1187@gmail.com>
2019-01-08 21:19:12 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -14,10 +14,10 @@ class Post
    end
    
    def author_name
-     if self.author.name != self.author.name
-       nil
-     else
+     if self.author.name == self.author.name
        self.author.name
+     else
+       nil
      end
    end
   
