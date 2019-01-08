2edd222dfdee7dea5b0b109cb777e29b9f524ce7
Sara Walker <sarawall1187@gmail.com>
2019-01-08 20:58:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -26,9 +26,11 @@ class Author
     @posts << single_post
     single_post.author = self
   end
+  
    def self.all
      @@all
    end
+   
    def self.post_count
      self.all.each do |author| 
       @@post_count += author.posts.count
